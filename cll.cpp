b810524457d2fc763c0be00013e8dd8d34fc9c73
AniruddhA-Omni <aniruddha.jana2020@vitstudent.ac.in>
2022-07-13 22:45:38 +0530

    Day 12: Circular Linked list

---
+++ cll.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+} *head;
+
+void Create(int A[], int n){
+    int i;
+    struct Node *t, *last;
+    head = new Node;
+    head->data = A[0];
+    head->next = head;
+    last = head;
+    for(i = 1;i<n;i++){
+        t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        last->next = t;
+        last =t;
+    }
+}
+
+
+void Display(struct Node *p){
+    head = p;
+    do{
+        cout<<p->data<<" ";
+        p = p->next;
+    } while (p != head);
+    cout<<endl;
+}
+
+int Length(struct Node* p){
+    int len = 0;
+    do{
+        len++;
+        p = p->next;
+    }while(p != head);
+    return len;
+}
+
+
+void Insert(struct Node* p, int index, int x){
+    struct Node* t;
+    int i;
+    if (index<0 || index > Length(head)) return;
+    if (index == 0){
+        t = new Node;
+         t->data =x;
+         if (head == NULL){
+            head =t;
+            head->next = head;
+         }else{
+            while(p->next != head) p = p->next;
+            p->next =t;
+            t->next =head;
+            head = t;
+         }
+    }else{
+        for(i = 0; i<index;i++){
+            p = p->next;
+            t = new Node;
+            t->data = x;
+            t->next = p->next;
+            head = t;
+        }
+    }
+}
+// bugs in insert
+
+int Delete(struct Node* p, int index){
+    struct Node* q;
+    int i, x;
+    if (index<0 || index > Length(head)) return -1;
+    if (index == 1){
+        while(p->next != head) p = p->next;
+        x = head->data;
+        if (head == p){
+            delete head;
+            head == NULL;
+        }else{
+            p->next = head->next;
+            delete head;
+            head = p->next;
+        }
+    }else{
+        for(i = 0; i< index-2; i++){
+            p = p->next;
+        }
+        q = p->next;
+        p->next= q->next;
+        x = q->data;
+        delete q;
+    }
+    return x;
+}
+
+int main(){
+    int A[] = {1,2,3,4,5,6};
+    Create(A,6);
+    Display(head);
+    // Insert(head,2,77);
+    // Display(head);
+    Delete(head,1);
+    Display(head);
+}
