492882d47eec775dc98db928e9cdce18f11719ed
AniruddhA-Omni <aniruddha.jana2020@vitstudent.ac.in>
2022-07-13 21:12:16 +0530

    Day 12: Linked List

--- ll1.cpp
+++ ll1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct Node{
     int data;
     struct Node *next;
-} *first = NULL;
+} *first = NULL, *second = NULL, *third = NULL;
 
 void create(int A[], int n){
     int i;
@@ -14,6 +14,21 @@ void create(int A[], int n){
     first->data = A[0];
     first->next = NULL;
     last = first;
+    for(i = 1; i < n; i++){
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+void create2(int A[], int n){
+    int i;
+    struct Node* t, *last;
+    second = new Node;
+    second->data = A[0];
+    second->next = NULL;
+    last = second;
     for(i = 1; i < n; i++){
         t = new Node;
         t->data = A[i];
@@ -154,15 +169,58 @@ void Reverse(struct Node* p){
         p = p->next;
         q->next = r; 
     }
+    first = q;
 }
 
+void Concat(struct Node *p, struct Node *q){
+    third = p;
+     while(p->next != NULL){
+        p = p->next;
+     }
+     p->next = q;
+}
+
+void Merge(struct Node *p, struct Node *q){
+    struct Node *last;
+    if (p->data < q->data){
+        third = last = p;
+        p = p->next;
+        third->next = NULL;
+    }else{
+        third = last = q;
+        q = q->next;
+        third->next = NULL;
+    }
+
+    while( p && q){
+        if (p->data < q->data){
+            last->next = p;
+            last = p;
+            p = p->next;
+            last->next = NULL;
+        }else{
+            last->next = q;
+            last = q;
+            q = q->next;
+            last->next = NULL;
+        }
+    }
+    if (p) last->next = p;
+    if (q) last->next = q;
+}
 
 int main(){
-    int A[] = {1, 2, 3, 4, 5}; 
+    int A[] = {1, 12, 30, 40, 55};
+    int B[] = {10,20,35,45,50}; 
     int n = sizeof(A)/sizeof(A[0]);
     create(A, n);
     Display(first);
-    Insert(first, 2, 10);
-    Display(first);
+    // Insert(first, 2, 10);
+    // Display(first);
+    create2(B,5);
+    Display(second);
+
+    Merge(first, second);
+    Display(third);
     return 0;
 }
